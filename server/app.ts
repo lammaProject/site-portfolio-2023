0a86a379bf21b2fe3695c49f3066342a9a90a631
Anton Tokarev <anton999@bk.ru>
2023-09-17 00:51:40 +0500

    fix

--- server/app.ts
+++ server/app.ts
@@ -9,13 +9,13 @@ const mainLandPath = path.resolve(__dirname, "../client/dist");
 app.use(express.static(mainLandPath));
 app.use(express.static(blanchPath));
 
-app.get("/blanchard", function (req, res) {
-  const indexPath = path.resolve(blanchPath, "index.html");
+app.get("/*", function (req, res) {
+  const indexPath = path.resolve(mainLandPath, "index.html");
   res.sendFile(indexPath);
 });
 
-app.get("/*", function (req, res) {
-  const indexPath = path.resolve(mainLandPath, "index.html");
+app.get("/blanchard", function (req, res) {
+  const indexPath = path.resolve(blanchPath, "index.html");
   res.sendFile(indexPath);
 });
 
